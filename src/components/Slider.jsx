01e974aa0bc623c57005218935a3d6a4063860e8
Julio Bandeira <88752929+JuniorBandeira7@users.noreply.github.com>
2023-08-09 23:12:40 -0300

    Fazendo o slider
    slider refeito usando useState e useEffect

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import style from './Slider.module.css'
 import imagem1 from './imgs/s1.jpg'
 import imagem2 from './imgs/s2.jpg'
+import imagem3 from './imgs/s3.jpg'
 
 
-function Slider(){
-    return(
-        <div id={style.dvslider}>
-            <img src={imagem1} alt="" />
-        </div>
-    )
-}
-export default Slider
+function Slider (){
+  const imagens = [imagem1, imagem2, imagem3];
+  const [imagemFundoIndex, setImagemFundoIndex] = useState(0);
+
+  function alterarImagemFundo(){
+    let novoIndice = (imagemFundoIndex + 1)
+    
+    if (novoIndice === imagens.length){
+        novoIndice = 0
+    }
+    setImagemFundoIndex(novoIndice)
+  };
+
+  useEffect(function () {
+    const intervalId = setInterval(alterarImagemFundo, 5000); // Trocar a cada 5 segundos
+
+    return function () {
+      clearInterval(intervalId); // Limpar o intervalo quando o componente é desmontado
+    };
+  }, [imagemFundoIndex]);
+
+  return (
+    <div id={style.dvslider} style={{backgroundImage: `url(${imagens[imagemFundoIndex]})`}}>
+    </div>
+  );
+};
+
+export default Slider;
